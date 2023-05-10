5b95ceda7871f07ec5ae72bd5ce8c670f0ec9704
DaeSungYoun <ydskingdom@gmail.com>
2023-05-11 01:05:10 +0900

    재고 감소 테스트

---
+++ stock-synchronized/src/test/java/com/example/stock/service/StockServiceTest.java
@@ -0,0 +1,50 @@
+package com.example.stock.service;
+
+import com.example.stock.domain.Stock;
+import com.example.stock.repository.StockRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.persistence.EntityManager;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class StockServiceTest {
+
+    @Autowired
+    StockRepository stockRepository;
+
+    @Autowired
+    StockService stockService;
+
+    @Autowired
+    EntityManager entityManager;
+
+    @BeforeEach
+    public void insert() {
+        Stock stock = new Stock(1L, 100L);
+
+        stockRepository.saveAndFlush(stock);
+    }
+
+    @AfterEach
+    public void delete() {
+        stockRepository.deleteAll();
+    }
+
+    @DisplayName("재고 감소 테스트")
+    @Test
+    public void decrease_test() {
+        stockService.decrease(1L, 1L);
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+        // 100 - 1 = 99
+
+        assertEquals(99, stock.getQuantity());
+    }
+}
