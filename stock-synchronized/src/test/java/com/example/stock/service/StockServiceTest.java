8dd0b2327c35b7721273f046a2db8925b3054175
DaeSungYoun <ydskingdom@gmail.com>
2023-05-15 21:21:19 +0900

    문제점

--- stock-synchronized/src/test/java/com/example/stock/service/StockServiceTest.java
+++ stock-synchronized/src/test/java/com/example/stock/service/StockServiceTest.java
@@ -11,6 +11,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.persistence.EntityManager;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -46,5 +50,29 @@ class StockServiceTest {
         // 100 - 1 = 99
 
         assertEquals(99, stock.getQuantity());
+    }
+
+    @Test
+    public void 동시에_100명이_주문() throws InterruptedException {
+        int threadCount = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    stockService.decrease(1L, 1L);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+
+        // 100 - (100 * 1) = 0
+        assertEquals(0, stock.getQuantity());
     }
 }
